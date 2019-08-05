694a43d313c335c8568b2dd2a35f07aae873ac4a
BHint <49994267+BHint@users.noreply.github.com>
2019-08-05 18:33:20 -0500

    Update employee_login.rb

--- Car app/employee_login.rb
+++ Car app/employee_login.rb
@@ -1,22 +1,34 @@
 @employee_numbers=[123123, 123456, 234234, 789789, 999999, 456789]
 
-puts "_____________________________________________________"
-puts "Enter your employee number"
-puts "_____________________________________________________"
-@emp_num = gets.to_i
+def login
+  puts "_____________________________________________________"
+  puts "Enter your employee number"
+  puts "_____________________________________________________"
+  @emp_num = gets.to_i
 
-case @emp_num
-  when 123123
-    puts "Welcome Kevin"
-  when 123456
-    puts "Welcome Ben"
-  when 234234
-    puts "Welcome Jamie"
-  when 789789
-    puts "Welcome Jerry"
-  when 999999
-    puts "Welcome Homer"
-  when 456789
-    puts "Welcome Jake"
-  else puts "Invalid ID, terminating session."
+  case @emp_num
+    when 123123
+      puts "Welcome Kevin"
+    when 123456
+      puts "Welcome Ben"
+    when 234234
+      puts "Welcome Jamie"
+    when 789789
+      puts "Welcome Jerry"
+    when 999999
+      puts "Welcome Homer"
+    when 456789
+      puts "Welcome Jake"
+    else
+      puts "Invalid ID"
+      puts "Try again? |Yes/No|"
+      answer = gets.chmop.upcase
+      if answer == "YES"
+        login
+      elsif answer == "NO"
+        puts "Thank you, have a nice day!"
+        exit
+      else
+        puts "Invalid input."
+  end
 end
