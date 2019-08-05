54e73a354c199d8ed69520273ea8eb2336a5479f
kevinjoey89 <kevinjoey@live.com>
2019-08-05 18:16:56 -0500

    updated with names

--- Car app/employee_login.rb
+++ Car app/employee_login.rb
@@ -1,14 +1,22 @@
-def login
-  @employee_numbers=[123123, 123456, 234234, 789789, 999999, 456789]
-  puts "_____________________________________________________"
-  puts "Enter your 6 digit employee number."
-  puts "_____________________________________________________"
-  @emp_num=gets.to_i
-  @success=0
-  if @employee_numbers.include? @emp_num
-    @success+=1
-    puts "Welcome Comrade"
-  else
-    puts "Invalid ID"
-  end
+@employee_numbers=[123123, 123456, 234234, 789789, 999999, 456789]
+
+puts "_____________________________________________________"
+puts "Enter your employee number"
+puts "_____________________________________________________"
+@emp_num = gets.to_i
+
+case @emp_num
+  when 123123
+    puts "Welcome Kevin"
+  when 123456
+    puts "Welcome Ben"
+  when 234234
+    puts "Welcome Jamie"
+  when 789789
+    puts "Welcome Jerry"
+  when 999999
+    puts "Welcome Homer"
+  when 456789
+    puts "Welcome Jake"
+  else puts "Invalid ID, terminating session."
 end
