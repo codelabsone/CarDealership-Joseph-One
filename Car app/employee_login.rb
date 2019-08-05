044bb6f7b69bc54cf553f4944da278b61e6209c8
BHint <49994267+BHint@users.noreply.github.com>
2019-08-05 18:56:13 -0500

    Update employee_login.rb

--- Car app/employee_login.rb
+++ Car app/employee_login.rb
@@ -1,34 +1,28 @@
-@employee_numbers=[123123, 123456, 234234, 789789, 999999, 456789]
-
 def login
+  @employee_numbers=[123123, 123456, 234234, 789789, 999999, 456789]
   puts "_____________________________________________________"
-  puts "Enter your employee number"
+  puts "Enter your 6 digit employee number."
   puts "_____________________________________________________"
-  @emp_num = gets.to_i
-
-  case @emp_num
-    when 123123
-      puts "Welcome Kevin"
-    when 123456
-      puts "Welcome Ben"
-    when 234234
-      puts "Welcome Jamie"
-    when 789789
-      puts "Welcome Jerry"
-    when 999999
-      puts "Welcome Homer"
-    when 456789
-      puts "Welcome Jake"
-    else
-      puts "Invalid ID"
-      puts "Try again? |Yes/No|"
-      answer = gets.chmop.upcase
-      if answer == "YES"
-        login
-      elsif answer == "NO"
-        puts "Thank you, have a nice day!"
-        exit
-      else
-        puts "Invalid input."
+  @emp_num=gets.to_i
+  @success=0
+  if @employee_numbers.include? @emp_num
+    @success+=1
+    case @emp_num
+      when 123123
+        puts "Welcome Kevin"
+      when 123456
+        puts "Welcome Ben"
+      when 234234
+        puts "Welcome Jamie"
+      when 789789
+        puts "Welcome Jerry"
+      when 999999
+        puts "Welcome Homer"
+      else 456789
+        puts "Welcome Jake"
+      end
+  else
+    puts "Invalid ID"
+    login
   end
 end
