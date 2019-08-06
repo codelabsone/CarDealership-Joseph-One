fedd18dc975cf8e6bf0407a29156bf97c7fdb9f6
BHint <49994267+BHint@users.noreply.github.com>
2019-08-05 19:11:38 -0500

    Update services.rb

--- Car app/services.rb
+++ Car app/services.rb
@@ -3,27 +3,22 @@ require_relative 'car_loan'
 require_relative 'customer_info'
 
 def services
-  puts "What would you like to view? [Inventory, Loan, Customer, Exit]"
+  puts "What would you like to view? |Inventory[1], Loan[2], Customer[3], Exit[4]|"
   while @user_input2 !="EXIT"
     @user_input2 = gets.chomp.upcase.to_s
-      if @user_input2 == 'INVENTORY'
+      if @user_input2 == 'INVENTORY'||@user_input2=='INV'||@user_input2=="1"
         puts "Current cars in inventory:"
         puts @car_lot
-      elsif @user_input2 == 'LOAN'
-        loan()
-      elsif @user_input2 == 'CUSTOMER'
-        cust_info()
-      elsif @user_input2 == 'EXIT'
+      elsif @user_input2 == 'LOAN'||@user_input2=="2"
+        loan
+      elsif @user_input2 == 'CUSTOMER'||@user_input2=="3"
+        cust_info
+      elsif @user_input2 == 'EXIT'||@user_input2=="4"
         puts "Thank you. Have a nice day!"
         exit
       else
         puts "Invalid choice."
       end
-        puts "\nWhat would you like to view? [Inventory, Loan, Customer, Exit]"
-        @user_input2= gets.chomp.upcase.to_s
-        if @user_input2 == "EXIT"
-          puts "Have a nice day!"
-          exit
-        end
-      end
+      puts "\nWhat would you like to view? |Inventory[1], Loan[2], Customer[3], Exit[4]|"
+    end
 end
