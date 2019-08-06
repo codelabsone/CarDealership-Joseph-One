8fc7b76b90850576a4198cc261aba50372d79ab1
BHint <49994267+BHint@users.noreply.github.com>
2019-08-05 19:57:20 -0500

    Update customer_info.rb

--- Car app/customer_info.rb
+++ Car app/customer_info.rb
@@ -13,12 +13,6 @@ end
 
 @full_db=[]
 
-def load_players(from_file)
-  File.readlines(from_file).each do |line|
-    puts line
-  end
-end
-
 def cust_info
   puts "Would you like to add customer info[1] or view the database[2]?"
   answer = gets.chomp.to_i
@@ -26,7 +20,7 @@ def cust_info
     cust1= Database.new(customer_info)
     @customer_array= [cust1]
     @full_db.push(@customer_array)
-    File.open("full_db.csv", "w+") do |f|
+    File.open("full_customer_db.csv", "w+") do |f|
       @full_db.each { |element| f.puts(element) }
     end
     puts @customer_array
