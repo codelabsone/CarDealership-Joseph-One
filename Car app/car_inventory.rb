04457b3baa1ef7df8c6d7df2311e16eb1878cf72
BHint <49994267+BHint@users.noreply.github.com>
2019-08-15 20:14:00 -0500

    Update car_inventory.rb

--- Car app/car_inventory.rb
+++ Car app/car_inventory.rb
@@ -1,4 +1,5 @@
 require_relative "car_questions"
+require_relative "services"
 require 'csv'
 array1=CSV.read("full_car_db.csv")
 class Car
@@ -13,24 +14,26 @@ class Car
   end
 end
 
-@full_car_db=array1.join(" ")
-
 def car_info
-  puts "Would you like to add car info[1] or view the car database[2]?"
+  @full_car_db = [File.readlines('full_car_db.csv')]
+  puts "Would you like to: \nadd car info[1] \nview the car database[2] \ngo back[3]?"
   answer = gets.chomp.to_i
   if answer == 1
     car1= Car.new(car_questions)
-    @car_array= [car1]
-    @full_car_db.push(@car_array)
-    File.open("full_car_db.csv", "w+") do |f|
-      @full_car_db.each { |element| f.puts(element) }
+    # @car_array= [car1]
+    # @full_car_db.push(@car_array)
+    File.open("full_car_db.csv", "a") do |f|
+      # @full_car_db.each { |element| f.puts(element) }
+      f.puts(car1)
     end
     puts @car_array
 
   elsif answer == 2
     puts @full_car_db
+  elsif answer == 3
+    services
   else
     puts "Invalid choice"
-    car_info
+    cust_info
   end
 end
